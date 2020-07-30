28371a72d0da3e768407d2588b1be7fefff9ebe9
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2020-07-30 16:06:54 +0530

    Added spline interpolation
    Method credits : https://www.particleincell.com/2012/bezier-splines/#demo

--- Utilities.py
+++ Utilities.py
@@ -8,6 +8,7 @@ import string
 import subprocess
 import more_itertools
 import itertools
+maketrans = str.maketrans
 from functools import reduce
 import networkx as nx
 import xml.etree.ElementTree as ET
@@ -29,6 +30,87 @@ from imageio import imwrite
 import math
 from skimage import transform
 import Data
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import spsolve
+from LinearSystem import *
+
+def smoothSpline(points) :
+    """
+    Create a smooth spline over a set of points.
+
+    Examples
+    --------
+    >>> points = [complex(0, 0), complex(10, 20), complex(20, 20)]
+    >>> path = smoothSpline(points)
+    >>> points = [path.point(t) for t in np.arange(0, 1, 0.1)
+    >>> x = [p.real for p in points]
+    >>> y = [p.imag for p in points]
+    >>> plt.plot(x, y)
+    >>> plt.show()
+    
+    Parameters
+    ----------
+    points: list
+        List of points in the complex plane where
+        the real coordinate is the x-coordinate 
+        and the imaginary coordinate is the y-coordinate
+    """
+    def linearSystem() : 
+        # Prepare linear system of equations.
+        eqns = []
+        for i in range(nSegs - 1) :
+            # First Constraint: P_(i + 1)_1 + P_i_2 = 2K_(i + 1)
+            v1x, v2x = f'P_{i + 1}_1_x', f'P_{i}_2_x'
+            v1y, v2y = f'P_{i + 1}_1_y', f'P_{i}_2_y'
+            ki_1 = points[i+1]
+            eqns.append(V[v1x] + V[v2x] == 2 * ki_1.real)
+            eqns.append(V[v1y] + V[v2y] == 2 * ki_1.imag)
+
+            # Second Constraint:
+            # -2P_(i + 1)_1 + P_(i + 1)_2 - P_i_1 + 2P_i_2 = 0
+            v3x, v4x = f'P_{i + 1}_2_x', f'P_{i}_1_x'
+            v3y, v4y = f'P_{i + 1}_2_y', f'P_{i}_1_y'
+            eqns.append(-2*V[v1x] + V[v3x] - V[v4x] + 2*V[v2x] == 0)
+            eqns.append(-2*V[v1y] + V[v3y] - V[v4y] + 2*V[v2y] == 0)
+
+        # 4 Boundary Condition constraints for open paths
+        # 2P_0_1 + P_1_1 = K_0 + 2 * K_1
+        k0, k1 = points[0:2]
+        eqns.append(2*V['P_0_1_x'] + V['P_1_1_x'] == k0.real + 2*k1.real)
+        eqns.append(2*V['P_0_1_y'] + V['P_1_1_y'] == k0.imag + 2*k1.imag)
+
+        # 2P_(n - 2)_1 + 7P_(n - 1)_1 = 8*K_(n - 1) + K_n
+        kn_1, kn = points[-2:]
+        eqns.append(2*V[v4x] + 7*V[v1x] == 8*kn_1.real + kn.real)
+        eqns.append(2*V[v4y] + 7*V[v1x] == 8*kn_1.imag + kn.imag)
+        
+        return constructLinearSystem(eqns, V.items(), np.array, csc_matrix)
+
+    def makePathFromSolutionVector (x) :
+        beziers = []
+        for i, knots in enumerate(zip(points, points[1:])) :
+            start, end = knots
+            P_i_1_x, P_i_1_y = V[f'P_{i}_1_x'], V[f'P_{i}_1_y']
+            P_i_2_x, P_i_2_y = V[f'P_{i}_2_x'], V[f'P_{i}_2_y']
+            cp1 = complex(x[VIdx[P_i_1_x]], x[VIdx[P_i_1_y]])
+            cp2 = complex(x[VIdx[P_i_2_x]], x[VIdx[P_i_2_y]])
+            beziers.append(svg.CubicBezier(start, cp1, cp2, end))
+        return svg.Path(*beziers)
+    
+    def initVariables () :
+        for i in range(n) : 
+            for j in range(1, 3) : 
+                xName, yName = f'P_{i}_{j}_x', f'P_{i}_{j}_y'
+                V[xName] = Variable(xName)
+                V[yName] = Variable(yName)
+
+    n = len(points) - 1
+    V = dict()
+    initVariables()
+    VIdx = dict(V.items(), range(len(V)))
+    A, b = linearSystem()
+    x = spsolve(A, b)
+    return makePathFromSolutionVector(x)
 
 class PerlinNoise () :
     """ 
@@ -1672,7 +1754,6 @@ def pathIntersectionArea (path1, path2, vbox) :
     intersect = img1 * img2
     n, m = intersect.shape
     fraction = intersect.sum() / (n * m)
-    print(fraction)
     return fraction
 
 def graphFromSvg (svgFile, graphFunction) : 
@@ -2135,3 +2216,20 @@ class AllPathDescriptorFunction () :
         return np.vstack(descs)
 
 if __name__ == "__main__" : 
+    points = [complex(0, 0), complex(10, 20), complex(20, 20)]
+    path = smoothSpline(points)
+    points = [path.point(t) for t in np.arange(0, 1, 0.1)
+    x = [p.real for p in points]
+    y = [p.imag for p in points]
+    plt.plot(x, y)
+    plt.show()
+    # xNoise = PerlinNoise(seed=0)
+    # yNoise = PerlinNoise(seed=1)
+    # doc = svg.Document('./drawing.svg')
+    # paths = doc.flatten_all_paths()
+    # vb = doc.get_viewbox()
+    # path = paths[0].path
+    # points = [path.point(t) for t in np.arange(0, 1, 0.55)]
+    # points = [p + complex(xNoise(p), yNoise(p)) for p in points]
+    # newPath = smoothSpline(points)
+    # singlePathSvg((newPath, paths[0].attrib), vb, 'output.svg')
