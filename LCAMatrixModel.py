Fixed bugs in greedy tree computation

LCAMatrixModel.py
@@ -8,7 +8,7 @@ from PathVAE import PathVAE
 import os
 from more_itertools import collapse
 from listOps import removeIndices
-from treeOps import treeFromNestedArray
+from treeOps import treeFromNestedArray, computeLCAMatrix, numNodes2Binarize
 from treeCompare import ted
 from tqdm import tqdm
 
@@ -45,19 +45,19 @@ class LCAMatrixModel (nn.Module) :
         pathSetj = graph['nodes'][j]['paths']
         return subMatrix(o, pathSeti, pathSetj).mean()
 
-    def greedyTree (self, x): 
+    def greedyTree (self, x, aggregate=torch.mean): 
         n, *_ = x.shape
         o = self.forward(x)
-        subtrees = list(map(lambda x : (x,), range(n)))
+        subtrees = list(map(lambda x : x, range(n)))
         while len(subtrees) > 1 : 
-            best, bestI, bestJ = 0, 0, 0
+            best, bestI, bestJ = -np.inf, None, None
             for i in range(len(subtrees)) :
                 for j in range(i + 1, len(subtrees)) :
                     subtree1 = subtrees[i]
                     subtree2 = subtrees[j]
-                    pathList1 = list(collapse(subtree1))
-                    pathList2 = list(collapse(subtree2))
-                    score = subMatrix(o, pathList1, pathList2).mean()
+                    pathList1 = list(collapse([subtree1]))
+                    pathList2 = list(collapse([subtree2]))
+                    score = aggregate(subMatrix(o, pathList1, pathList2))
                     if score > best : 
                         best = score
                         bestI = i
@@ -81,9 +81,10 @@ class LCAMatrixModel (nn.Module) :
         o = self.nn2(x).view(n, n)
         return o
 
-def distanceFromGroundTruth (model, t) : 
-    t_ = model.greedyTree(t.descriptors)
-    return ted(t, t_)
+def distanceFromGroundTruth (model, t, aggregator) : 
+    t_ = model.greedyTree(t.descriptors, aggregator)
+    a = ted(t, t_)
+    return ted(t, t_) / (t.number_of_nodes() + t_.number_of_nodes())
 
 def test (model, t) :
     t_ = (model.greedyTree(t.descriptors))
@@ -101,14 +102,20 @@ if __name__ == "__main__" :
     # Load all the data
     testDir = commonConfig['test_directory']
     testData = SVGDataSet(testDir, 'adjGraph', 10, useColor=False)
-    testData.toTensor(cuda=True)
-    pathVAE = PathVAE(config)
-    model = LCAMatrixModel(pathVAE, config['sampler'])
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    MERGE_OUTPUT = os.path.join(BASE_DIR, "results", "lca_merge")
-    state_dict = torch.load(os.path.join(MERGE_OUTPUT, 'training_end.pth'))
-    model.load_state_dict(state_dict['model'])
-    model.to("cuda")
-    scores = [distanceFromGroundTruth(model, t) for t in tqdm(testData)]
-    print(scores)
-    print(np.mean(scores), np.std(scores))
+    delta = [numNodes2Binarize(t) for t in testData]
+    dist = [n / (2 * t.number_of_nodes() + n) for t, n in zip(testData, delta)]
+    print(np.mean(dist), np.std(dist))
+    # testData.toTensor(cuda=True)
+    # pathVAE = PathVAE(config)
+    # model = LCAMatrixModel(pathVAE, config['sampler'])
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # MERGE_OUTPUT = os.path.join(BASE_DIR, "results", "lca_merge")
+    # state_dict = torch.load(os.path.join(MERGE_OUTPUT, 'training_end.pth'))
+    # model.load_state_dict(state_dict['model'])
+    # model.to("cuda")
+    # aggregators = [torch.min, torch.max, torch.mean]
+    # things = []
+    # for a in aggregators : 
+    #     scores = [distanceFromGroundTruth(model, t, a) for t in tqdm(testData)]
+    #     things.append((np.mean(scores), np.std(scores)))
+    # print(things)

SVGData.py
@@ -7,7 +7,7 @@ from descriptor import relbb, equiDistantSamples, pathAttr
 from functools import reduce
 import numpy as np
 from graphOps import contractGraph
-from treeOps import findRoot, treeApplyChildrenFirst, lca, setNodeDepths, maxDepth
+from treeOps import findRoot, treeApplyChildrenFirst, lca, setNodeDepths, maxDepth, computeLCAMatrix
 from torchvision import transforms as T
 import torch
 from svgIO import getTreeStructureFromSVG
@@ -55,15 +55,7 @@ class SVGData (nx.DiGraph) :
             self.descriptors = [equiDistantSamples(p.path, docViewBox, nSamples=nSamples) for p in paths]
         self._computeBBoxes(self.root)
         self._pathSet2Tuple()
-        self._computeLCAMatrix()
-
-    def _computeLCAMatrix(self) : 
-        self.lcaMatrix = np.zeros((self.nPaths, self.nPaths))
-        self.maxDepth = maxDepth(self)
-        setNodeDepths(self)
-        for i in range(self.nPaths) : 
-            for j in range(self.nPaths) : 
-                self.lcaMatrix[i, j] = self.nodes[lca(self, i, j)]['depth'] / self.maxDepth
+        computeLCAMatrix(self)
 
     def _nodeId2PathId (self, n) : 
         assert self.out_degree(n) == 0, "Function called with internal node"

treeCompare.py
@@ -70,7 +70,6 @@ def ted (t1, t2) :
     W = pd.DataFrame(data=-np.inf * np.ones((n, m)), columns=t2.nodes, index=t1.nodes)
     for x, y in itertools.product(t1.nodes, t2.nodes) :
         computeW(x, y)
-    print(W)
     # Use the W matrix to calculate the edit distance
     v = variableMatrix(t1.nodes, t2.nodes)
     prob = LpProblem("TreeEditDistance", LpMaximize)
@@ -407,5 +406,5 @@ if __name__ == "__main__" :
     # Load all the data
     testDir = commonConfig['test_directory']
     testData = SVGDataSet(testDir, 'adjGraph', 10, useColor=False)
-    dists = [ted_(t, t) for t in tqdm(testData)]
+    dists = [ted(t, t) for t in tqdm(testData)]
     print(np.mean(dists))