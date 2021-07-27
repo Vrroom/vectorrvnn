3f34c542187f4d24e0a4fdea262d8a6e141903ac
vrroom <sumit.chaturvedi@gmail.com>
2021-07-27 23:23:10 +0530

    Added requirement for pathfinder_rasterizer

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setuptools.setup(
     between vector elements. Includes both hand
     written and learnt heuristics.
     """,
+    dependency_links=['https://github.com/Vrroom/pathfinder_rasterizer/tarball/clipPathFix#egg=pathfinder_rasterizer-0.1.0'],
     install_requires=INSTALL_REQUIRES,
     extras_require={
         'test': TEST_REQUIRES + INSTALL_REQUIRES,
     },
-
 )
