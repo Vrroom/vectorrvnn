be50bec351ab3a44151868c0e9037ec9a671fe79
vrroom <sumit.chaturvedi@gmail.com>
2021-08-09 11:03:31 +0530

    Fixed gain for kaiming initialization
    Earlier, it was set as per leaky_relu but we are using relu.

--- vectorrvnn/trainutils/initializer.py
+++ vectorrvnn/trainutils/initializer.py
@@ -14,7 +14,7 @@ def getInitializer(opts, init_gain=0.02):
             elif init_type == 'xavier':
                 init.xavier_normal_(m.weight.data, gain=init_gain)
             elif init_type == 'kaiming':
-                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                init.kaiming_normal_(m.weight.data, nonlinearity='relu')
             elif init_type == 'orthogonal':
                 init.orthogonal_(m.weight.data, gain=init_gain)
             else:
