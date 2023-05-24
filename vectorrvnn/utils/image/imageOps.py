e438e9cd9a90db50c5f57d7ece1a22d7b8532542
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2023-05-24 15:14:09 +0530

    Convert image to bytes and back

--- vectorrvnn/utils/image/imageOps.py
+++ vectorrvnn/utils/image/imageOps.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import base64, io
 
 def imsave (arr, fname) : 
     """ utility for saving numpy array """ 
@@ -26,3 +27,17 @@ def imgArrayToPILRGB (arr) :
     assert(arr.dtype == np.uint8)
     chanType = "RGBA" if arr.shape[2] == 4 else "RGB"
     return Image.fromarray(arr, chanType).convert("RGB")
+
+ENCODING = 'utf-8'
+
+def bytes2PIL(b64Str):
+    imgdata = base64.b64decode(str(b64Str))
+    return Image.open(io.BytesIO(imgdata))
+
+def PIL2byteStr(img):
+    img = np.array(img)
+    buffered = io.BytesIO()
+    image = Image.fromarray(img)
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
+    return img_str.decode(ENCODING)
