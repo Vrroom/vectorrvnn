53cfee036030ca981ff170e7f503e5fa212576a0
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2023-09-09 02:58:37 +0530

    Added decorator for avoiding processed tasks

--- vectorrvnn/utils/parallel/pTools.py
+++ vectorrvnn/utils/parallel/pTools.py
@@ -1,6 +1,8 @@
 from tqdm import tqdm
 import multiprocessing as mp
 from functools import wraps
+import os
+import hashlib
 
 def pmap(function, items, chunksize=None) : 
     """ parallel mapper using Pool with progress bar """
@@ -47,3 +49,44 @@ def safe_run(f):
             print(f"Exception occurred while running {f.__name__}: {e}")
             return None
     return wrapped
+
+def skip_if_processed(task_file="completed_tasks.txt"):
+    """
+    Decorator to skip function execution if it has been previously processed. 
+
+    This is useful in data processing if program crashes and you want to restart
+    without doing everything all over again.
+    """
+
+    # Initialize a set to hold completed task hashes
+    completed_task_hashes = set()
+
+    # Load completed task hashes from file
+    if os.path.exists(task_file):
+        with open(task_file, "r") as f:
+            completed_task_hashes = set(line.strip() for line in f.readlines())
+
+    def compute_hash(args):
+        """Compute SHA-256 hash for the given arguments."""
+        args_str = str(args)
+        return hashlib.sha256(args_str.encode()).hexdigest()
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            task_hash = compute_hash(args)
+
+            # If the task is not yet completed, run it and mark as completed
+            if task_hash not in completed_task_hashes:
+                result = func(*args, **kwargs)
+
+                # Update completed task hashes and save to file
+                completed_task_hashes.add(task_hash)
+                with open(task_file, "a") as f:
+                    f.write(f"{task_hash}\n")
+
+                return result
+            else:
+                print(f"Task with hash {task_hash} has already been processed, skipping.")
+        return wrapper
+    return decorator
